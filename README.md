d3ee8a52fb9d2ea3ab367f34dbb9ce48e84377f5
VIJAYSADHUKHAN22 <74904553+VIJAYSADHUKHAN22@users.noreply.github.com>
2020-12-22 01:16:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PROC21A
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,49 @@
+var wall,thinkness;
+var bullet,speed,weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  
+speed=random(223,321);
+weight=random(30,52);
+thinkness=random(22,83);
+bullet=createSprite(50,200,50,10);
+
+bullet.velocityX=speed;
+
+wall=createSprite(1200,200,thinkness, height/2);
+bullet.shapeColor=color(255);
+
+wall.shapeColor=color(80,80,80);
+}
+
+function draw() {
+  background(0);  
+
+if(hasCollided(bullet,wall))
+{
+  bullet.velocityX=0;
+  var deformation=0.5*weight*speed*speed/(thinkness*thinkness*thinkness);
+
+  if (deformation>10){
+    wall.shapeColor=color(255,0,0);
+  }
+
+  if (deformation<10){
+    wall.shapeColor=color(0,255,0);
+  }
+}
+  
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true;
+  }
+  return false;
+}
